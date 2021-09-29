6f98ae377e0b26d02e7aaed00ee314e5fc03373d
Julian David Villegas <julian.villegas00@gmail.com>
2021-09-28 21:24:37 -0500

    Se crea el proyecto Frontend y se inicia con la configuracion del ambiente. Se crea Bootstrap.

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Build Sign Up & Login UI Template in React</h3>
+    </div>
+  );
+}
+
+export default App;
