ca7f8b004a0280544a10dccfe7793c4c4415b268
Julian David Villegas <julian.villegas00@gmail.com>
2021-09-28 21:29:22 -0500

    Se crean los componentes de Registro y de Login.

---
+++ frontend/src/components/signup.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Componente de Registro</h3>
+            </div>
+        );
+    }
+}
